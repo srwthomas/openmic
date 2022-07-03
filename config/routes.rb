2d5d89f232274d540db07c34aff106909da154d0
scott thomas <srwthomas1@gmail.com>
2022-07-03 19:14:11 +0200

    newr

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   resources :posts
 
-  mount RailsAdmin::Engine => '/adminlmom91fnfxw6uuke8y8pzbnueiq35hg8m1buqh5w24dvu2x0y8pjriv53jd7l182', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/adminlmom91fnfxw6uuke8y8pzbnueiq35hg8m1buqh5w24dvumykme3lbip1xb361ynk2s2mzt7pmlq2sokol6d3a2x0y8pjriv53jd7l1822a0mmepo9fqjlhojza0c', as: 'rails_admin'
 
 end
