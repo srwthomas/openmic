c950a281e241d05cf179b4826acf3af97322b8b6
scott thomas <srwthomas1@gmail.com>
2022-07-03 17:37:01 +0200

    added select function

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :name, :instrument, :genre, presence: true
+  GENRES = ["Alternative", "Blues", "Country", "Folk", "Jazz", "Mixed", "Pop", "Rock", "Other" ]
 
 end
