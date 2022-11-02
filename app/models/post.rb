9f68b0b5c1c1622195bdad046f7822b7ddddf938
scott thomas <srwthomas1@gmail.com>
2022-11-02 10:27:07 +0100

    typo ,

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :name, :instrument, :genre, presence: true
-  GENRES = ["Alternative", "Blues", "Country", "Folk", "Jazz", "Mixed", "Pop", "Rock", "Rock & Roll" "Other Genre" ]
+  GENRES = ["Alternative", "Blues", "Country", "Folk", "Jazz", "Mixed", "Pop", "Rock", "Rock & Roll", "Other Genre" ]
 
 end
